dfd7fc00c69236c37d3d61a132de6aab747c1f7a
eldarabramovich <eldarabramovich090@gmail.com>
2024-02-18 22:26:40 +0200

    more changes

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -2,8 +2,50 @@
 const admin = require('firebase-admin');
 const db = require('../firebase/firebaseAdmin.js');
 
+// const addAssignmentToClass = async (req,res) => {
+//     const {classId, title, description, dueDate, createdBy } = req.body;
+//     try {
+//       const assignmentRef = db.collection('assignments').doc();
+//       await assignmentRef.set({
+//         classId: classId,
+//         title: title,
+//         description: description,
+//         dueDate: dueDate,
+//         createdBy: createdBy,
+//         createdAt: admin.firestore.Timestamp.now()
+//       });
+//       console.log('Assignment added successfully!');
+//     } catch (error) {
+//       console.error('Error adding assignment:', error);
+//     }
+//   }
 
+const addAssignmentToClass = async (req,res) => {
 
+    const {title, description, classId , lastdate} = req.body;
+
+    if (!classId || !title || !description , lastdate) {
+        return res.status(400).send("Missing data!");
+    }
+
+    try {
+      const db = admin.firestore();
+      const classRef = db.collection('class').doc(classId);
+
+      await classRef.update({
+        assignments: admin.firestore.FieldValue.arrayUnion({
+            title,
+            description,
+            dueDate: admin.firestore.Timestamp.fromDate(new Date(dueDate)),
+            lastdate
+        })
+    });
+      console.log('Assignment saved successfully!');
+    } catch (error) {
+      console.error('Error saving assignment:', error);
+    }
+  }
+  
 
 const addAdmin = async (req, res) => {
     const { username,password } = req.body; // Example fields
@@ -34,16 +76,17 @@ const addTeacher = async (req, res) => {
     
     try {
         const db = admin.firestore();
-        const teacherRef = db.collection('teachers').doc(); // Create a new doc in 'teachers' collection
+        const teacherRef = db.collection('users').doc(); // Create a new doc in 'teachers' collection
         await teacherRef.set({
             username,
             password,
             subject,
             fullname,
             email,
+            role:"teacher",
             grades:[]
         });
-        
+
         res.status(200).send('Teacher added successfully');
 
     } catch (error) {
@@ -60,12 +103,13 @@ const addStudent = async (req, res) => {
     }
     try {
         const db = admin.firestore();
-        const studentRef = db.collection('students').doc(); // Create a new doc in 'students' collection
+        const studentRef = db.collection('users').doc(); // Create a new doc in 'students' collection
         await studentRef.set({
             username,
             password,
             fullname,
             grade,
+            role:"student",
             assignment:[]
         });
         res.status(200).send('Student added successfully');
@@ -76,7 +120,8 @@ const addStudent = async (req, res) => {
 };
 
 const addClass = async (req, res) => {
-    const { grade , number  } = req.body; // Example fields
+    const { grade , number  } = req.body; 
+    // Example fields
     if(!grade || !number ){
         res.status(500).send("missing data !");
     }
@@ -98,7 +143,7 @@ const addClass = async (req, res) => {
     }
 };
 
-const addassigtoclass = async (req, res) => {
+const addgradestoteacher = async (req, res) => {
     const { username,password } = req.body; // Example fields
     if(!username || !password ){
         res.status(500).send("missing data !");
