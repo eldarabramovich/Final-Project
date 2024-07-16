da4023a601337b844eca9638b20a4c122300324e
eldarabramovich <eldarabramovich090@gmail.com>
2024-07-16 18:05:05 +0300

    parent adding pages

--- frontend/lib/Parent/ParentHomeScreen.dart
+++ frontend/lib/Parent/ParentHomeScreen.dart
@@ -1,135 +1,218 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:frontend/config.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:frontend/models/parentmodel.dart'; // Assuming you have a parent model
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:frontend/Parent/ParentChildGrade.dart'; // Replace with actual import paths
+import 'package:frontend/Parent/ParentSendMessagePage.dart'; // Replace with actual import paths
+// import 'package:frontend/Parent/ParentEventsPage.dart'; // Replace with actual import paths
+import 'package:frontend/Parent/ParentCalendarPage.dart'; // Replace with actual import paths
+import 'package:google_fonts/google_fonts.dart';
 
 class ParentDashboard extends StatefulWidget {
   final Parent parent;
   final Children selectedChild;
 
-  ParentDashboard({required this.parent, required this.selectedChild});
+  const ParentDashboard({required this.parent, required this.selectedChild});
 
   @override
   _ParentDashboardState createState() => _ParentDashboardState();
 }
 
 class _ParentDashboardState extends State<ParentDashboard> {
-  List<Map<String, dynamic>> finalGrades = [];
-  List<Map<String, dynamic>> assignmentGrades = [];
-  bool isLoading = true;
-
   @override
   void initState() {
     super.initState();
-    fetchData();
   }
 
-  Future<void> fetchData() async {
-    await fetchFinalGrades();
-    await fetchAssignmentGrades();
-    setState(() {
-      isLoading = false;
-    });
-  }
-
-  Future<void> fetchFinalGrades() async {
-    try {
-      final response = await http.post(
-        Uri.parse('http://${Config.baseUrl}/parent/finalGrades'),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(<String, String>{
-          'studentId': widget.selectedChild.id,
-          'fullname': widget.selectedChild.fullname,
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        List<dynamic> gradesJson = json.decode(response.body);
-        setState(() {
-          finalGrades =
-              gradesJson.map((json) => json as Map<String, dynamic>).toList();
-        });
-        print('Final grades fetched: $finalGrades');
-      } else {
-        Fluttertoast.showToast(msg: "Failed to load final grades");
-        print('Failed to load final grades: ${response.body}');
-      }
-    } catch (e) {
-      Fluttertoast.showToast(msg: "Failed to load final grades: $e");
-      print('Error fetching final grades: $e');
-    }
-  }
-
-  Future<void> fetchAssignmentGrades() async {
-    try {
-      final response = await http.post(
-        Uri.parse('http://${Config.baseUrl}/parent/assignmentGrades'),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(<String, String>{
-          'studentId': widget.selectedChild.id,
-          'fullName': widget.selectedChild.fullname,
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        List<dynamic> gradesJson = json.decode(response.body);
-        setState(() {
-          assignmentGrades =
-              gradesJson.map((json) => json as Map<String, dynamic>).toList();
-        });
-        print('Assignment grades fetched: $assignmentGrades');
-      } else {
-        Fluttertoast.showToast(msg: "Failed to load assignment grades");
-        print('Failed to load assignment grades: ${response.body}');
-      }
-    } catch (e) {
-      Fluttertoast.showToast(msg: "Failed to load assignment grades: $e");
-      print('Error fetching assignment grades: $e');
-    }
+  void userLogOut(BuildContext context) {
+    // Implement logout logic here
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ברוך הבא ${widget.parent.fullname}'),
+        title: const Text('ברוך הבא '),
+        actions: [
+          IconButton(
+            onPressed: () => userLogOut(context),
+            icon: const Icon(Icons.logout),
+          )
+        ],
         backgroundColor: Colors.blue.shade800,
       ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : Column(
+      body: Column(
+        children: [
+          // Part 1: Basic information about the parent
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 10.0,
+            padding: const EdgeInsets.all(20),
+            color: Colors.blue.shade800,
+            child: Column(
               children: [
-                Expanded(
-                  child: ListView(
-                    children: [
-                      ...finalGrades.map((grade) {
-                        return ExpansionTile(
-                          title: Text(grade['subjectName']),
-                          subtitle: Text('ציון סופי: ${grade['finalGrade']}'),
-                          children: [
-                            ...assignmentGrades
-                                .where((ag) =>
-                                    ag['subjectName'] == grade['subjectName'])
-                                .map((ag) {
-                              return ListTile(
-                                title: Text(ag['assignmentName']),
-                                subtitle: Text('ציון: ${ag['grade']}'),
-                              );
-                            }).toList(),
-                          ],
-                        );
-                      }).toList(),
-                    ],
-                  ),
-                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          " Welcome Parent ",
+                          style: GoogleFonts.notoSerifHebrew(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text(
+                          "שלום",
+                          style: GoogleFonts.notoSerifHebrew(
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
               ],
             ),
+          ),
+
+          // Part 2: Navigation cards
+          Expanded(
+            child: Container(
+              color: Colors.blue.shade800,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  color: Color(0xFFF4F6F7),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  ),
+                ),
+                child: ListView(
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ParentHomeCard(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ParentChildGrade(
+                                  parent: widget.parent,
+                                  selectedChild: widget.selectedChild,
+                                ),
+                              ),
+                            );
+                          },
+                          icon: 'asset/icons/result.svg',
+                          title: "View Grades",
+                        ),
+                        ParentHomeCard(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ParentCalendarPage(
+                                  userId: widget.parent.id,
+                                  children: widget.selectedChild,
+                                ),
+                              ),
+                            );
+                          },
+                          icon: 'asset/icons/timetable.svg',
+                          title: "לוח שנה",
+                        ),
+                      ],
+                    ),
+                    ParentHomeCard(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ParentSendMessagePage(
+                              userId: widget.parent.id,
+                              selectedChild: widget.selectedChild,
+                            ),
+                          ),
+                        );
+                      },
+                      icon: 'asset/icons/result.svg',
+                      title: "הודעות",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ParentHomeCard extends StatelessWidget {
+  const ParentHomeCard({
+    Key? key,
+    required this.onPressed,
+    required this.icon,
+    required this.title,
+    this.color = Colors.black,
+    this.elevation = 4.0,
+  }) : super(key: key);
+
+  final VoidCallback onPressed;
+  final String icon;
+  final String title;
+  final Color color;
+  final double elevation;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        margin: const EdgeInsets.only(top: 40.0),
+        width: MediaQuery.of(context).size.width / 2.5,
+        height: MediaQuery.of(context).size.height / 6,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 247, 247, 247),
+          borderRadius: BorderRadius.circular(20.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 4,
+              offset: const Offset(0, 2), // Adjust the offset as needed
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              icon,
+              height: 55.0,
+              width: 55.0,
+              color: color,
+            ),
+            Text(
+              title,
+              style: GoogleFonts.notoSerifHebrew(
+                color: const Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
